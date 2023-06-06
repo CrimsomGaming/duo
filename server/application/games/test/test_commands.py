84f7af6a9f50e8eb5b5a843e28c5f040717ec944
Marconi Soares <marconi.developer@outlook.com.br>
2023-06-06 18:31:38 -0300

    test(server/games): add tests for seed_games command

---
+++ server/application/games/test/test_commands.py
@@ -0,0 +1,39 @@
+from io import StringIO
+from django.test import TestCase
+from django.core.management import call_command
+from games.models import *
+
+
+class SeedGamesTestCase(TestCase):
+    def setUp(self):
+        self.out = StringIO()
+        call_command("seed_users", stdout=self.out)
+    
+    def get_game_models_count(self):
+        return {
+            'user': User.objects.count(),
+            'game': Game.objects.count(),
+            'weekday': Weekday.objects.count(),
+            'ann': Announcement.objects.count(),
+        }
+
+    def test_command_working_on_empty_db(self):
+        self.assertDictEqual(                                # Before running
+            self.get_game_models_count(),
+            {'user': 30, 'game':0, 'weekday': 0, 'ann': 0}
+        )
+
+        call_command("seed_games", stdout=self.out)
+        self.assertDictEqual(
+            self.get_game_models_count(),
+            {'user':30, 'game':10, 'weekday': 7, 'ann': 300}
+        )
+
+    def test_command_should_allways_return_the_same_amount_of_data(self):
+        call_command("seed_games", stdout=self.out)
+        call_command("seed_games", stdout=self.out)
+        self.assertDictEqual(
+            self.get_game_models_count(),
+            {'user':30, 'game':10, 'weekday': 7, 'ann': 300}
+        )
+
